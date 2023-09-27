42610dfb369af6b42d7fc42514c73edb61baa42d
screwax <screwaxx@gmail.com>
2023-09-27 15:24:16 +0300

    hw

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const age = 17
+console.log(age)
+const namee = "James"
+console.log(namee)
+const isStudent = true
+console.log(isStudent)
+const myString = "goiteens"
+console.log(myString)
+const myNumber = 11
+console.log(myNumber + 10)
+const myNull = null
+console.log(myNull)
+
+const user = prompt("Як тебе звати?")
+console.log(user)
+
+if (confirm("Пiдтвердити?") === true) {
+    alert("Дякую за підтвердження!")
+} else {
+    alert("Дію відмінено!")
+}
